0dd4bc3e4fa6265bf749fa6b23dab9fd9f16bc03
Ebrar <16155052@mersin.edu.tr>
2023-03-30 16:46:20 +0300

    event page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,10 +1,41 @@
+import { useRouter } from 'next/router';
+import { Fragment } from 'react';
+import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
 function EventDetailPage(){
+    const router = useRouter();
+    
+   const eventId= router.query.eventId;
+   const event =getEventById(eventId);
+
+   function handleGoBack(){
+       router.push('/');
+   }
+
+   if(!event){
+         return <p>No event found!</p>
+    }
     return (
-        <div>
-            <h1>Event Detail Page</h1>
-        </div>
+        <Fragment>
+             <EventSummary title={event.title} />
+                <EventLogistics
+                 date={event.date}
+                  address={event.location}
+                     image={event.image}
+                        imageAlt={event.title}   
+                  />
+                <EventContent>
+                    <p>{event.description}</p>
+                </EventContent>
+                <button style={{display: 'block',padding:"10px", margin: '0 auto', backgroundColor: 'blue', color: 'white'}}  onClick={handleGoBack}>Show All Events</button>
+
+            </Fragment>
     )
 
 }
 
 export default EventDetailPage;
+
+
